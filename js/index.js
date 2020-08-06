93249064756142139d413c4c1200fb1c6f836775
Msyoki <msyokimutua@gmail.com>
2020-08-06 13:24:20 +0300

    add font styling ,edit form,add js files and link

--- js/index.js
+++ js/index.js
@@ -1,31 +1,54 @@
-function Pizza(size, toppings1,toppings2,toppings3,toppings4){
+function Pizza(size, crust,topping,total){
   this.size = size;
-  this.toppings1 = toppings1;
-  this.toppings2 = toppings2;
-  this.toppings3 = toppings3;
-  this.toppings4 = toppings4;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
 };
+Pizza.prototype.order =function () {
+  return ("You have ordered " + this.size + "with " + this.crust + "crusting" + "and " + this.toppings + "topping");
+}
+function TotalPrice (price, quantity, delivery,toppings,crust) {
+  this.price= price;
+  this.size=quantity;
+  this.delivery=delivery;
+  this.toppings=toppings;
+  this.crust=crust;
+  };
+  TotalPrice.prototype.finalTotal = function () {
+  return ( this.price + this.delivery + this.toppings + this.crust )* this.quantity ;
+  }; 
+  
+  var sizeName = ["small","medium","large"]
+  var sizePrice = [350,500,1000];
+  var crustPrice = [150,200,180];
+  var toppPrice = [200,300,180,200,180];   
 
-Pizza.prototype.price = function(){
-  var total = this.toppings1 + this.toppings2 + this.toppings3 + this.toppings4 + this.size;
-  return total;
-};
+  var userSize = $("#size option:selected").val();
+  var userCrusting = parseInt($("input#crust").val());
+  var userTopping = parseInt($("input#toppings").val());
+  var userFlavour = $("input#flavour").val();
+  var userDelivery = $("input#location").val();
+  var userNumber = parseInt($("input#quantity").val());
+
+  var realSize = sizePrice[userSize];
+  var realCrusting =crustingPrice[userCrusting];
+  var realTopping = toppingPrice[userTopping];
+  var realName = sizeName[userSize];
+
+  order = new Pizza(userSize,userCrust,userTopping,userNumber);
+  newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
+
+  $("#btn1").click(function(){
+    if(userDelivery === " "){
+      DeliveryCost = 0
+      newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
+      $("ol#list").append(newTotal.finalTotal())
+    }
+  })
+  
+     //$("p").append("<b>Appended text</b>");
 
 
-$(document).ready(function(){
-  $("#pizzaOrder").submit(function(event){
-    event.preventDefault();
-    var size = parseFloat($("#size").val());
-    var toppings1 = parseFloat($("#toppings1").val());
-    var toppings2 = parseFloat($("#toppings2").val());
-    var toppings3 = parseFloat($("#toppings3").val());
-    var toppings4 = parseFloat($("#toppings4").val());
 
-    var userPizza = new Pizza(size,toppings1,toppings2,toppings3,toppings4);
-    
-    $("#customerOrder").slideToggle(500);
-    $("ul").append("<li>" + "$" + userPizza.price().toFixed(2) + "</li>");
-    $("li").remove();
-    $("ul").append("<li>" + "$" + userPizza.price().toFixed(2) + "</li>");
-  });
-});
+
+
