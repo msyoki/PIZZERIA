5e75c0d6bd368f79bac3a5450b55e82ecfdcc4a1
msyoki <msyokimutua@gmail.com>
2020-11-29 13:23:00 +0300

    updated project

--- js/index.js
+++ js/index.js
@@ -1,54 +1,44 @@
-function Pizza(size, crust,topping,total){
-  this.size = size;
-  this.crust = crust;
-  this.topping = topping;
-  this.total = total;
-};
-Pizza.prototype.order =function () {
-  return ("You have ordered " + this.size + "with " + this.crust + "crusting" + "and " + this.toppings + "topping");
-}
-function TotalPrice (price, quantity, delivery,toppings,crust) {
-  this.price= price;
-  this.size=quantity;
-  this.delivery=delivery;
-  this.toppings=toppings;
-  this.crust=crust;
-  };
-  TotalPrice.prototype.finalTotal = function () {
-  return ( this.price + this.delivery + this.toppings + this.crust )* this.quantity ;
-  }; 
-  
-  var sizeName = ["small","medium","large"]
-  var sizePrice = [350,500,1000];
-  var crustPrice = [150,200,180];
-  var toppPrice = [200,300,180,200,180];   
 
-  var userSize = $("#size option:selected").val();
-  var userCrusting = parseInt($("input#crust").val());
-  var userTopping = parseInt($("input#toppings").val());
-  var userFlavour = $("input#flavour").val();
-  var userDelivery = $("input#location").val();
-  var userNumber = parseInt($("input#quantity").val());
+$(document).ready(function() {
+    $('#scroll').hide();
+})    
 
-  var realSize = sizePrice[userSize];
-  var realCrusting =crustingPrice[userCrusting];
-  var realTopping = toppingPrice[userTopping];
-  var realName = sizeName[userSize];
+$(document).ready(function() {
+    $('#summary').hide();
+})
+$(document).ready(function() {
+  var deliveryPrice = 0;
+  $('#checkout').click(function() {
+      var checkValue = document.getElementById('delivery').value;
+      if (checkValue === '400') {
+          deliveryPrice += 400;
+          var location = prompt('Enter delivery location:');
+          var contact = prompt('Enter contact number:');
+          alert('Delivery Price is Ksh' + deliveryPrice)
+          alert('Pizza will be delivered at' + ' ' + location);
+          alert('Contact number:' + ' ' + contact);
+      }     
+  });
 
-  order = new Pizza(userSize,userCrust,userTopping,userNumber);
-  newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
+  $('form#pizza').submit(function(event) {
+    var pizzaSize = parseFloat(document.getElementById('size').value);
+    var pizzaCrust = parseFloat(document.getElementById('crust').value);
+    var pizzaTopping = parseFloat(document.getElementById('toppings').value);
+    var pizzaQuantity = parseFloat(document.getElementById('number').value);
+    var pizzaDelivery = parseFloat(document.getElementById('delivery').value);
+    var total = (pizzaSize + pizzaCrust + pizzaTopping + pizzaDelivery)*pizzaQuantity;
+    
+    document.getElementById('displayTotal').innerHTML = total 
+    console.log(total);
 
-  $("#btn1").click(function(){
-    if(userDelivery === " "){
-      DeliveryCost = 0
-      newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
-      $("ol#list").append(newTotal.finalTotal())
-    }
-  })
-  
-     //$("p").append("<b>Appended text</b>");
+    document.getElementById('seeYourSizeChoice').innerHTML = pizzaSize;
+    document.getElementById('seeYourCrustChoice').innerHTML = pizzaCrust;
+    document.getElementById('seeYourToppingsChoice').innerHTML = pizzaTopping;
+    document.getElementById('seeYourDeliveryChoice').innerHTML = pizzaDelivery;
+    
+    $('#summary').show();
+    $('#scroll').show();
 
-
-
-
-
+    event.preventDefault();
+  });
+})
